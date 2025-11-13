21d66f743a22122b5003ade3df0644538bd95c16
dario medina <dariomedina99@gmail.com>
2025-11-12 22:16:36 -0300

    se modifico archivo index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,10 @@
 import express from "express";
 import { conectarDB } from "./db.js";
+import medicosRouter from "./medicos.js";
+import pacientesRouter from "./pacientes.js";
+import turnosRouter from "./turnos.js";
+import usuariosRouter from "./usuarios.js";
+
 import cors from "cors";
 
 
@@ -16,6 +21,13 @@ app.use(cors());
 app.get("/", (req, res) => {
     res.send("Hola mundo!");
 });
+
+
+app.use("/medicos", medicosRouter);
+app.use("/pacientes", pacientesRouter);
+app.use("/turnos", turnosRouter);
+app.use("/usuarios", usuariosRouter);
+
 
 app.listen(port, () => {
     console.log(`La aplicación esta funcionando en el puerto ${port}`);
